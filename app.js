3dffe764bbc2cf718003c1bdc30e81219fe9e5da
Nate Bruggeman <njbruggeman@gmail.com>
2021-06-24 16:17:18 -0500

    fixed font awesome, fully rendering list

--- app.js
+++ app.js
@@ -5,3 +5,38 @@ const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter')
 const taskInput = document.querySelector('#task')
+
+
+// load all event listeners
+loadEventListeners();
+
+function loadEventListeners(){
+    form.addEventListener('submit', addTask);
+}
+
+//adding a task, if nothing is present give an alert
+function addTask(e){
+    if(taskInput.value === ''){
+        alert('Add a Task')
+    }
+    //create list element
+    const li = document.createElement('li');
+    // give list element a class
+    li.className = 'collection-item';
+    // create text in the list item and append to li
+    li.appendChild(document.createTextNode(taskInput.value))
+    // Create new link element (X next to list item for deletion)
+    const link = document.createElement('a');
+    link.className = 'delete-item secondary-content';
+    link.innerHTML = '<i class="fa fa-check"></i>'
+    //append link to the li
+    li.appendChild(link);
+
+    //append li to ul
+    taskList.appendChild(li)
+
+    e.preventDefault()
+}
+
+
+
