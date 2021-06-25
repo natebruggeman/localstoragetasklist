04286babce7af710117025abd8df21aac7b599a8
Nate Bruggeman <njbruggeman@gmail.com>
2021-06-25 16:12:45 -0500

    deleting items on ui and localstorage

--- app.js
+++ app.js
@@ -95,8 +95,26 @@ function removeTask(e){
     if(e.target.parentElement.classList.contains('delete-item')){
         if(confirm('Are you sure?')) {
             e.target.parentElement.parentElement.remove()
+            //remove from localstorage
+            removeFromLocalStorage(e.target.parentElement.parentElement);
         }
     }
+}
+
+function removeFromLocalStorage(taskItem){
+    let tasks;
+    if(localStorage.getItem('tasks') === null){
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    tasks.forEach(function(task, index){
+        if(taskItem.textContent === task){
+            tasks.splice(index, 1)
+        }
+    });
+    localStorage.setItem('tasks', JSON.stringify(tasks))
 }
 
 function clearTasks(e){
